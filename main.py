d7f8dbed3d4bfdafa76eb1d65032b41f2d0c754d
Bradley Selee <bselee@clemson.edu>
2020-10-30 20:43:21 -0400

    Setting up main file and opened basic window

---
+++ main.py
@@ -0,0 +1,32 @@
+"""
+ main.c
+ Bradley Selee
+ ECE 6310 Fall 2020
+ Graduate Project
+
+ Purpose:
+
+ Assumptions:
+    1) 
+
+ Bugs:
+    1) Running this project in a git bash with "python main.py"
+        - fix: Run in git bash using "py main.py"
+
+"""
+
+import tkinter as tk
+
+#class Window:
+ #   def init
+
+def main():
+
+    window = tk.Tk()
+
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
+
+
