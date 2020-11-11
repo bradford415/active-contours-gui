a4546fc31df0caca7a4f5391b3e3d26f6684c0f2
Bradley Selee <bselee@clemson.edu>
2020-11-11 00:08:41 -0500

    changing the way I run the gui

--- main.py
+++ main.py
@@ -13,29 +13,60 @@
     1) Running this project in a git bash with "python main.py"
         - fix: Run in git bash using "py main.py"
 
+ Dependencies:
+    Run these commands on a windows command line
+    1) pip install pillow
+
  Defintions:
     Canvas - main screen to work with
     Frame - organize widgets
 
 """
 import tkinter as tk
+import tkinter.filedialog
+import PIL
+from PIL import ImageTk, Image
+import os
 
 HEIGHT = 600
 WIDTH = 700
 
+class Application(tk.Tk):
 
-class Application:
-    
     def __init__(self, master):
-        self.master = master
-        self.init_window()
-        self.init_menu()
+        tk.Tk.__init__(self)
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        
+        frame = HomePage(container, self)
+        frame.grid(row=HEIGHT, column=WIDTH, sticky="nsew")
+        
+        self.frames = {}
+        self.frames[HomePage] = frame
+        self.show_frame(HomePage)
     
+    def show_frame(self, controller):
+        frame = self.frames[controller]
+        frame.tkraise()
+
+
+class HomePage(tk.Frame):
+    
+    def __init__(self, master, controller):
+        tk.Frame.__init__(self, master)
+        self.master = master
+        #self.init_window()
+        #self.init_menu()
+        self.root_dir = os.path.dirname(os.path.abspath(__file__))
+        self.picture_dir = os.path.join(self.root_dir, "pictures")
+
     def init_window(self):
         self.master.title("Active Contours")
         self.canvas = tk.Canvas(self.master, height=HEIGHT, width=WIDTH)
         self.canvas.pack()
-        self.background_frame = tk.Frame(self.master, bg="#6200EE")
+        self.background_frame = tk.Frame(self.master, bg="#8fbaff")
         self.background_frame.place(relwidth=1, relheight=1)
 
         
@@ -46,15 +77,26 @@ class Application:
         actions = tk.Menu(menu)
         menu.add_cascade(label='Actions', menu=actions)
 
-        actions.add_command(label='Load Image', command=self.test_func)
+        actions.add_command(label='Load Image', 
+                                command=lambda: controller.show_frame(ImageViewer))
         actions.add_command(label='Exit', command=self.exit_client)
 
-    def test_func(self):
-        print("Test")
+    
     
     def exit_client(self):
         exit()
 
+class ImageViewer(tk.Frame):
+
+    def __int__(self, master, controller):
+        load_image()
+        
+    def load_image(self):
+        file_name = tk.filedialog.askopenfilename(initialdir=self.picture_dir, 
+                                                    title="Select a Picture", 
+                                                    filetypes=[("all images","*.pnm *.png *.jpg")])
+        my_image = ImageTk.PhotoImage(Image.open(file_name))
+        my_image_label = tk.Label(image=my_image).pack()
 
 def main():
 
